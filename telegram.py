50654c15746d0e64674401ef65214df0b0e9947d
vladimirzavrazhnov <streamvz@gmail.com>
2016-09-18 19:29:25 +0300

    Добавили обработку Hi

--- telegram.py
+++ telegram.py
@@ -5,8 +5,12 @@ def handle(msg):
 
     chat_id = msg['from']['id']
     command = msg['text']
+
     if command == '/on':
         bot.sendMessage(chat_id,str('Okey On!'))
+
+    if command == '/hi':
+        bot.sendMessage(chat_id,str('Hi. How do you do?'))
 
 bot = telepot.Bot('236615992:AAEChw-Cf2lgHlWXfMG70XXmVcrRCKsC6BI')
 bot.message_loop(handle)
