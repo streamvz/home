e8229b7629f0c66c7b9018c70fa30a82c0b1c3b5
vladimirzavrazhnov <streamvz@gmail.com>
2016-09-27 14:53:59 +0300

    Send message in cyrcle
    Off serial port

--- telegram.py
+++ telegram.py
@@ -2,6 +2,9 @@ import time
 import telepot
 #import serial
 
+isSend = '-'
+chatID = '-'
+
 def handle(msg):
 
     chat_id = msg['from']['id']
@@ -23,8 +26,7 @@ def handle(msg):
 
 #ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-isSend = '-'
-chatID = '-'
+
 
 bot = telepot.Bot('236615992:AAEChw-Cf2lgHlWXfMG70XXmVcrRCKsC6BI')
 bot.message_loop(handle)
