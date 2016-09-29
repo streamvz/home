ce6f286bb71ecbf1eb9365e8a0f3db7e21dc095f
vladimirzavrazhnov <streamvz@gmail.com>
2016-09-29 11:40:04 +0300

    Telegram API Main New File

--- telegramapi.py
+++ telegramapi.py
@@ -1,3 +1,18 @@
-import telebot
+import time
+import telepot
 
-bot = telebot.TeleBot('236615992:AAEChw-Cf2lgHlWXfMG70XXmVcrRCKsC6BI')
+def handle(msg):
+
+    chat_id = msg['from']['id']
+    command = msg['text']
+
+    if command == '/on':
+        bot.sendMessage(chat_id,str('Okey On!'))
+
+
+bot = telepot.Bot('236615992:AAEChw-Cf2lgHlWXfMG70XXmVcrRCKsC6BI')
+bot.message_loop(handle)
+
+while 1:
+
+    time.sleep(10)
