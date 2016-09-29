3443a96f0fbbf37f082aefb048e59a7c0291bf41
vladimirzavrazhnov <streamvz@gmail.com>
2016-09-29 12:08:05 +0300

    Telegram API Main New File

--- telegramapi.py
+++ telegramapi.py
@@ -1,23 +1,3 @@
-import time
-import telepot
+import telebot
 
-# def handle(msg):
-#
-#     chat_id = msg['from']['id']
-#     command = msg['text']
-#
-#     if command == '/on':
-#         bot.sendMessage(chat_id,str('Okey On!'))
-
-
-bot = telepot.Bot('236615992:AAEChw-Cf2lgHlWXfMG70XXmVcrRCKsC6BI')
-
-@bot.message_handler(content_types=["text"])
-def repeatmessages(message):
-    bot.send_message(message.chat.id, message.text)
-
-#bot.message_loop(handle)
-
-while 1:
-
-    time.sleep(10)
+bot = telebot.TeleBot('236615992:AAEChw-Cf2lgHlWXfMG70XXmVcrRCKsC6BI')
