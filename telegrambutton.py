d2c413643c6b9f99673c22db2e1cdfa70fde3ab7
vladimirzavrazhnov <streamvz@gmail.com>
2017-03-27 18:39:23 +0300

    telegram button

--- telegrambutton.py
+++ telegrambutton.py
@@ -1,8 +1,9 @@
 import telepot
 import types
 
-bot = telepot.Bot('236615992:AAEChw-Cf2lgHlWXfMG70XXmVcrRCKsC6BI')
 
 @bot.message_handler(content_types=["text"])
 def any_msg(message):
     keyboard = types.InlineKeyboardMarkup()
+
+bot = telepot.Bot('236615992:AAEChw-Cf2lgHlWXfMG70XXmVcrRCKsC6BI')
